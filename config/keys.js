8aa33338183e95456c417bac64a703f16721123b
mehdizademilad <milad.mehdizade1394@gmail.com>
2020-01-22 15:19:52 +0330

    Config directory added with keys.js

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+const dbUserName = 'userName';
+const dbPassword = 'password';
+const dbName = 'urlMapper';
+const dbUrl = 'mongodb://' + '127.0.0.1' + ':27017';
+mongoConnectionString = `${dbUrl}/${dbName}`;
+
+module.exports = {
+    dbUrl : mongoConnectionString
+};
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { dbUrl } = require('./config/keys');
 
 const app = express();
 const port = 3003;
 
-mongoose.connect('mongodb://127.0.0.1:27017/urlMapper', {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true });
 
 const routes = require('./api/routes');
 app.use(bodyParser.json());
