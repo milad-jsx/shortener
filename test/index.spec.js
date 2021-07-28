c7f4c2dae4e9c13b45fdbee4efbffe2f03ec13a5
Milad Mehdizade <milad.mehdizade1394@gmail.com>
2021-07-28 18:22:14 +0430

    add test

--- test/index.spec.js
+++ test/index.spec.js
@@ -11,6 +11,21 @@ describe('.'.repeat(30) + ' Shortener ' + '.'.repeat(30), function () {
             expect(uuid).not.be.undefined;
         });
 
+        it('should shorten a url reversibly', async () => {
+            const { ShortenerService } = require('../services');
+
+            const originalURL = 'www.example.com';
+
+            const shortenedURLResponse = await ShortenerService.shortIt(originalURL);
+            expect(shortenedURLResponse.isSucceeded).to.be.true;
+            expect(shortenedURLResponse.shortenedURL).to.not.be.null;
+
+            const reversedURL = await ShortenerService.reverseShortenedURL(shortenedURLResponse);
+            expect(reversedURL.isSucceeded).to.be.true;
+            expect(reversedURL.originalURL).to.not.be.null;
+            expect(reversedURL.originalURL).to.be.equal(originalURL);
+        });
+
     })
 
 });
