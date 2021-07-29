f8034a6e4661c7bf7319f6f01b1afd687ddee99c
Milad Mehdizade <milad.mehdizade1394@gmail.com>
2021-07-29 12:02:17 +0430

    add validation for empty request from swagger

--- helper/url.js
+++ helper/url.js
@@ -3,6 +3,10 @@ module.exports = {
         let result = null;
 
         try {
+            if (!urlInRequest || urlInRequest === '{url}') {
+                return null;
+            }
+
             const url = require('url');
 
             const userURL = url.parse(urlInRequest);
